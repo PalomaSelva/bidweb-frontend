3c5707d4669c21ee7cb1536bf0a16e3eaad88038
PalomaSelva <palomaselva1@email.com>
2025-04-03 20:13:26 -0300

    feat: refatorar componente Input para suportar refs e integrar validação de formulário no login

--- src/pages/auth/log-in.tsx
+++ src/pages/auth/log-in.tsx
@@ -3,8 +3,38 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const loginForm = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, "A senha deve ter pelo menos 8 caracteres")
+    .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
+    .regex(/[a-z]/, "A senha deve conter pelo menos uma letra minúscula")
+    .regex(/[0-9]/, "A senha deve conter pelo menos um número")
+    .regex(/[\W_]/, "A senha deve conter pelo menos um caractere especial"),
+});
+
+// Converte o tipo do zod para o tipo do TS
+type SignInFormData = z.infer<typeof loginForm>;
 
 export function LogIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInFormData>({
+    resolver: zodResolver(loginForm),
+  });
+
+  async function handleLogin(data: SignInFormData) {
+    console.log(data);
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+  }
   return (
     <>
       <Helmet title="Login" />
@@ -17,21 +47,29 @@ export function LogIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleLogin)}>
             <div className="flex flex-col gap-2">
               <Label htmlFor="email" className="font-medium">
                 E-mail
               </Label>
-              <Input id="email" type="email" placeholder="exemplo@email.com" />
+              <Input
+                type="email"
+                placeholder="exemplo@email.com"
+                {...register("email")}
+              />
             </div>
 
             <div className="flex flex-col gap-2">
               <Label htmlFor="password" className="font-medium">
                 Senha
               </Label>
-              <Input id="password" type="password" placeholder="********" />
+              <Input
+                type="password"
+                placeholder="********"
+                {...register("password")}
+              />
             </div>
-            <Button className="w-full" type="submit">
+            <Button disabled={isSubmitting} className="w-full" type="submit">
               Entrar
             </Button>
           </form>
