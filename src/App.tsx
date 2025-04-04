4514278b54b925d16e25204c193ad12475ab6b84
PalomaSelva <palomaselva1@email.com>
2025-04-04 01:28:14 -0300

    feat: adicionar ThemeProvider para gerenciamento de temas e integrar ao componente App

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme-providers";
 export function App() {
   return (
     <HelmetProvider>
-      <Toaster />
-      <Helmet titleTemplate="%s | SalesTrack" />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="dark" storageKey="sales-theme">
+        <Toaster />
+        <Helmet titleTemplate="%s | SalesTrack" />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
