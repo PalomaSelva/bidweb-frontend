bef5db5939cbf72bd527fd3ebe5b05add6914b79
PalomaSelva <palomaselva1@email.com>
2025-04-06 19:21:47 -0300

    feat: implementar funcionalidade para obter receita mensal e exibir no cartão de receita

--- src/pages/app/dashboard/month-revenue-card.tsx
+++ src/pages/app/dashboard/month-revenue-card.tsx
@@ -1,21 +1,50 @@
+import { getMonthRevenue } from "@/api/get-month-revenue";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
 import { DollarSign } from "lucide-react";
+import { MetricCardSkeleton } from "./metric-card-skeleton";
 
 export function MonthRevenueCard() {
+  const { data: monthRevenue } = useQuery({
+    queryKey: ['month-revenue'],
+    queryFn: getMonthRevenue,
+  })
   return (
     <Card className="gap-0.5">
       <CardHeader className="flex items-center justify-between pb-2">
         <CardTitle className="text-base">Receita total (mês)</CardTitle>
         <DollarSign size={16} className="text-muted-foreground" />
       </CardHeader>
-      <CardContent className="">
-        <span className="text-2xl font-bold tracking-tight">R$ 10.000,00</span>
-        <p className="text-muted-foreground mt-1 text-sm">
-          <span className="mr-0.5 text-emerald-600 dark:text-emerald-400">
-            +10%
-          </span>{" "}
-          em relação ao mês anterior
-        </p>
+      <CardContent className="space-y-1">
+        {monthRevenue ? (
+          <>
+            <span className="text-2xl font-bold tracking-tight">
+              {(monthRevenue.receipt / 100).toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              })}
+            </span>
+            <p className="text-xs text-muted-foreground">
+              {monthRevenue.diffFromLastMonth >= 0 ? (
+                <>
+                  <span className="text-emerald-500 dark:text-emerald-400">
+                    +{monthRevenue.diffFromLastMonth}%
+                  </span>{' '}
+                  em relação ao mês passado
+                </>
+              ) : (
+                <>
+                  <span className="text-rose-500 dark:text-rose-400">
+                    {monthRevenue.diffFromLastMonth}%
+                  </span>{' '}
+                  em relação ao mês passado
+                </>
+              )}
+            </p>
+          </>
+        ) : (
+          <MetricCardSkeleton />
+        )}
       </CardContent>
     </Card>
   );
