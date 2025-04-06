d811510ae31195d27a026f9b36929b2215cf508a
PalomaSelva <palomaselva1@email.com>
2025-04-06 11:48:02 -0300

    feat: adicionar autenticação no Axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,15 @@
 import { env } from "@/env";
 import axios from "axios";
+import { getAuthToken } from "./auth";
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
 });
 
+api.interceptors.request.use((config) => {
+  const token = getAuthToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
