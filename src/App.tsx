05abb018fbe90fa978060b65eb6515d965283b18
PalomaSelva <palomaselva1@email.com>
2025-04-06 10:13:50 -0300

    feat: integrar o QueryClientProvider ao App

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,17 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/theme/theme-providers";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 export function App() {
   return (
     <HelmetProvider>
       <ThemeProvider defaultTheme="dark" storageKey="sales-theme">
         <Toaster />
         <Helmet titleTemplate="%s | SalesTrack" />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   );
