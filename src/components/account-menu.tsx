88780d664b571d7e522b486b896b010caf6aea9d
PalomaSelva <palomaselva1@email.com>
2025-04-06 11:47:51 -0300

    feat: integrar perfil do usuário no menu

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -8,8 +8,23 @@ import {
 } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { ChevronDown, LogOut } from "lucide-react";
+import { getProfile } from "@/api/get-profile";
+import { useQuery } from "@tanstack/react-query";
+import { removeAuthToken } from "@/lib/auth";
+import { useNavigate } from "react-router";
 
 export function AccountMenu() {
+  const navigate = useNavigate();
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile,
+  })
+
+
+  function handleSignOut() {
+    removeAuthToken();
+    navigate('/sign-in');
+  }
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -17,19 +32,19 @@ export function AccountMenu() {
           variant="outline"
           className="flex items-center gap-2 select-none"
         >
-          SalesTrack
+          {profile?.name}
           <ChevronDown size={16} />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col p-1.5">
-          <span className="font-medium">Paloma Selva</span>
+          <span className="font-medium">{profile?.name}</span>
           <span className="text-muted-foreground text-xs">
-            palomaselva@gmail.com
+            {profile?.email}
           </span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={handleSignOut}>
           <LogOut size={16} />
           Sair
         </DropdownMenuItem>
